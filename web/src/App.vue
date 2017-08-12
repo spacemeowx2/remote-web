d8752edf94ced92a7d38c76e8e36a547cf3d440c
imspace <imspace@vip.qq.com>
2017-08-13 01:29:08 +0800

    外壳改进

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,59 +1,108 @@
 <template lang="pug">
-  #app
-    md-toolbar
-      h1.md-title(style="flex: 1") 我的设备
-      md-button.md-icon-button.md-warn
-        md-icon info
-    .main-content
-      md-tabs
-        md-tab(md-label='家')
-          md-card(md-with-hover)
-            md-card-header
-              .md-title 25℃
-              .md-subhead 上次更新 1秒前
-            md-card-content
-              | 温度
-          md-card(md-with-hover)
-            md-card-header
-              .md-title 70%
-              .md-subhead 上次更新 1秒前
-            md-card-content
-              | 湿度
-          md-card(md-with-hover)
-            md-card-header
-              .md-title 空调
-            md-card-content
-              md-list
-                md-list-item(@click='doCommand') 开机
-                md-list-item(@click='doCommand') 关机
-          md-card(md-with-hover)
-            md-card-header
-              .md-title 电视
-            md-card-content
-              md-list
-                md-list-item(@click='doCommand') 开机
-                md-list-item(@click='doCommand') 关机
-          //- md-list
-          //-   md-list-item.type 空调
-          //-   md-list-item 开机
-          //-   md-list-item 关机
-          //-     md-divider
-          //-   md-list-item.type 电视
-          //-   md-list-item 开机
-          //-   md-list-item 关机
+#app
+  md-toolbar
+    h1.md-title(style="flex: 1") 我的设备
+    md-button.md-icon-button.md-warn
+      md-icon info
+    md-button.md-icon-button(@click='openDialog')
+      md-icon settings
+  .main-content
+    md-tabs
+      md-tab(v-for='i in deviceList', :key='i.id', :md-label='i.name')
+        device(
+          @command='doCommand',
+          :sensorTypes='devices[i.id].sensorTypes',
+          :supportCommand='devices[i.id].supportCommand'
+        )
+  md-dialog-prompt(
+    v-if='promptShow'
+    md-title='设置服务器WebSocket',
+    md-ok-text='确定',
+    md-cancel-text='取消',
+    v-model='wsServer',
+    ref='wsServer',
+    @close='onClose'
+  )
+</md-dialog-prompt>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
+import Device from './components/Device'
+function getTime () {
+  return (new Date()).getTime()
+}
 export default {
   name: 'app',
   components: {
-    Hello
+    Device
+  },
+  data () {
+    return {
+      wsServer: 'ws://192.168.199.152:3000/client',
+      promptShow: true,
+      deviceList: [{
+        id: 'a',
+        name: '家'
+      }],
+      devices: {
+        a: {
+          sensorTypes: [{
+            typeID: 1,
+            typeName: '温度',
+            unit: '℃'
+          }, {
+            typeID: 2,
+            typeName: '湿度',
+            unit: '%'
+          }],
+          supportCommand: [{
+            typeID: 1,
+            typeName: '测试类别',
+            commands: [{
+              cmdID: 1,
+              cmdName: '开机'
+            }, {
+              cmdID: 2,
+              cmdName: '关机'
+            }]
+          }, {
+            typeID: 2,
+            typeName: '测试类别2',
+            commands: [{
+              cmdID: 1,
+              cmdName: '开机'
+            }, {
+              cmdID: 2,
+              cmdName: '关机'
+            }]
+          }]
+        }
+      },
+      activeDeviceId: null,
+      now: getTime(),
+      nowInterval: null
+    }
+  },
+  created () {
+    this.nowInterval = setInterval(() => {
+      this.now = getTime()
+    }, 1000)
+  },
+  destroyed () {
+    clearInterval(this.nowInterval)
   },
   methods: {
-    doCommand () {
-      //
+    doCommand (typeId, cmdId) {
+      console.log(typeId, cmdId)
+    },
+    openDialog () {
+      this.promptValue = this.wsServer
+      this.$refs.wsServer.open()
+    },
+    onClose (type) {
+      this.promptShow = false
+      setTimeout(() => { this.promptShow = true }, 1) // fucking workround, to reset value in textbox
+      console.log('onClose', type)
     }
   }
 }
@@ -70,9 +119,6 @@ body {
   -moz-osx-font-smoothing: grayscale;
   width: 100%;
   height: 100%;
-}
-.md-card {
-  margin: 10px 0;
 }
 .type {
   font-weight: bold;
