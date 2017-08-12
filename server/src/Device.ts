23166a6ef36013321b3a0397fea1049da32e2cb5
imspace <imspace@vip.qq.com>
2017-08-12 13:22:08 +0800

    web

--- server/src/Device.ts
+++ server/src/Device.ts
@@ -1,6 +1,22 @@
 import * as WebSocket from 'ws'
+import * as Protocol from './Protocol'
 export class Device {
   constructor (public ws: WebSocket) {
-    
+    ws.on('message', (data) => {
+      if (typeof data === 'string') {
+        try {
+          let obj = JSON.parse(data)
+          this.onMessage(obj)
+        } catch (e) {
+          //
+        }
+      }
+    })
+  }
+  onMessage (data: Protocol.Package) {
+    if (data.type === 'Handshake') {
+      let pkg = data as Protocol.PHandshake
+      
+    }
   }
 }
