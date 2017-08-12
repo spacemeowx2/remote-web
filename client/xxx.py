ed4ccc9ef1e1fdd883ad7285046c5e0906a0c85f
dazzlepppp <dazzlepppp@outlook.com>
2017-08-12 22:06:50 +0800

    add Command

--- client/xxx.py
+++ client/xxx.py
@@ -1,6 +1,23 @@
 import websocket
 import json
 
+class Command:
+
+    def __init__(self):
+        self.commands = []
+    
+    def add(self,command):
+        self.commands.append(command)
+    
+    def dumps(self):
+        s = []
+        for i in range(len(self.commands)):
+            s.append({
+                'cmdID': i,
+                'cmdName': self.commands[i]
+            })
+        return json.dumps(s)
+
 class SurppotCommand:
 
     def __init__(self):
@@ -10,12 +27,12 @@ class SurppotCommand:
     def add(self, typeName, command):
         return self.commands
     
-    def dumps():
+    def dumps(self):
         return 1
 
-s = '{"TypeID":123}'
-z = json.loads(s)
-x = json.dumps(z)
+commands = Command()
+commands.add("test1")
+commands.add("test2")
+commands.add("test3")
 
-print z
-print x 
+print commands.dumps()
