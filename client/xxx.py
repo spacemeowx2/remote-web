4171866cb0f883c620973645dc389b96eb69fd80
dazzlepppp <dazzlepppp@outlook.com>
2017-08-12 20:58:48 +0800

    client

---
+++ client/xxx.py
@@ -0,0 +1,16 @@
+import websocket
+import json
+
+class SurppotCommand:
+
+    def __init__(self):
+        self.commands = []
+        self.nextID = 0
+    
+    def add(self, typeName, command):
+        self.commands
+    
+
+package
+
+print json.loads(s)
