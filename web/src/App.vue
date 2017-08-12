559aefca6d2c24861d19a2e64a5d91b61f44ea39
imspace <imspace@vip.qq.com>
2017-08-12 16:39:26 +0800

    一个壳子

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,8 +1,46 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
-  </div>
+<template lang="pug">
+  #app
+    md-toolbar
+      h1.md-title(style="flex: 1") 我的设备
+      md-button.md-icon-button.md-warn
+        md-icon info
+    .main-content
+      md-tabs
+        md-tab(md-label='家')
+          md-card(md-with-hover)
+            md-card-header
+              .md-title 25℃
+              .md-subhead 上次更新 1秒前
+            md-card-content
+              | 温度
+          md-card(md-with-hover)
+            md-card-header
+              .md-title 70%
+              .md-subhead 上次更新 1秒前
+            md-card-content
+              | 湿度
+          md-card(md-with-hover)
+            md-card-header
+              .md-title 空调
+            md-card-content
+              md-list
+                md-list-item(@click='doCommand') 开机
+                md-list-item(@click='doCommand') 关机
+          md-card(md-with-hover)
+            md-card-header
+              .md-title 电视
+            md-card-content
+              md-list
+                md-list-item(@click='doCommand') 开机
+                md-list-item(@click='doCommand') 关机
+          //- md-list
+          //-   md-list-item.type 空调
+          //-   md-list-item 开机
+          //-   md-list-item 关机
+          //-     md-divider
+          //-   md-list-item.type 电视
+          //-   md-list-item 开机
+          //-   md-list-item 关机
 </template>
 
 <script>
@@ -12,17 +50,31 @@ export default {
   name: 'app',
   components: {
     Hello
+  },
+  methods: {
+    doCommand () {
+      //
+    }
   }
 }
 </script>
 
-<style>
+<style lang="less">
+body {
+  width: 100%;
+  height: 100%;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+}
+.md-card {
+  margin: 10px 0;
+}
+.type {
+  font-weight: bold;
 }
 </style>
--- web/src/main.js
+++ web/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import VueMaterial from 'vue-material'
 import App from './App'
+import 'vue-material/dist/vue-material.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueMaterial)
 
 /* eslint-disable no-new */
 new Vue({
