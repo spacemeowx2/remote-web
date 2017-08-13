b0d3332de63cf8ec658b4e293a1c2bbbbe028ca8
imspace <imspace@vip.qq.com>
2017-08-13 20:17:54 +0800

    持续更新时间

--- web/src/components/Device.vue
+++ web/src/components/Device.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
 .device
   div(v-if='ready')
-    md-card(md-with-hover, v-for='sensorType in sensorTypes', :key='sensorType.typeID')
+    md-card(md-with-hover, v-for='sensor in sensors', :key='sensor.typeID')
       md-card-header
-        .md-title {{getSensorVal(sensorType.typeID)}}{{sensorType.unit}}
-        .md-subhead 上次更新 {{getSensorPastTime(sensorType.typeID)}}
+        .md-title {{sensor.value}}{{sensor.unit}}
+        .md-subhead 上次更新 {{getSensorPastTime(sensor.pastTime)}}
       md-card-content
-        | {{sensorType.typeName}}
+        | {{sensor.typeName}}
     md-card(md-with-hover, v-for='catelog in supportCommand', :key='catelog.typeID')
       md-card-header
         .md-title {{catelog.typeName}}
@@ -30,18 +30,27 @@ export default {
     getSensorVal (id) {
       return this.sensorValues ? this.sensorValues[id].d : 'N/A'
     },
-    getSensorPastTime (id) {
-      if (!this.sensorValues) {
+    getSensorPastTime (pastSec) {
+      if (pastSec === null) {
         return '从未'
       }
-      let pastSec = this.sensorValues[id].t - util.nowSec
       if (pastSec < 1) {
-        return '现在'
+        pastSec = 1
       }
       if (pastSec < 60) {
         return `${pastSec}秒前`
       }
       return `${Math.floor(pastSec / 60)}分钟前`
+    }
+  },
+  computed: {
+    sensors () {
+      return this.sensorTypes.map(i => {
+        const val = this.sensorValues && this.sensorValues[i.typeID]
+        i.value = val ? val.d : 'N/A'
+        i.pastTime = val ? util.nowSec - val.t : null
+        return i
+      })
     }
   },
   props: ['sensorTypes', 'sensorValues', 'supportCommand', 'ready']
