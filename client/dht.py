b0bddd172739f52d73c5e38b091f19f9ba7dafa3
dazzlepppp <dazzlepppp@outlook.com>
2017-08-13 18:01:11 +0800

    add dht*.py

---
+++ client/dht.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import dht11
+import time
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+# read data using pin 14
+instance = dht11.DHT11(pin = 24)
+
+while True:
+    result = instance.read()
+
+    if result.is_valid():
+        print("Temperature: %d C" % result.temperature)
+        print("Humidity: %d %%" % result.humidity)
+    else:
+        print("Error: %d" % result.error_code)
+    time.sleep(1.5)
